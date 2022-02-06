3c63c041e53b6f79ea5b412bcc07b0d8e9accce2
Stepan <potapovstepana@gmail.com>
2022-02-07 03:33:09 +0700

    Add project

---
+++ SwiftUI-Calculator/SwiftUI_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CalculatorApp.swift
+//  SwiftUI-Calculator
+//
+//  Created by Stepan Potapov on 07.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
