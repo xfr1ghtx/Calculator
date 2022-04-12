ff921af2a4c8d5cacb0eb7d7308c80c253a61e3b
fr1ght <potapovstepana@gmail.com>
2022-04-12 23:52:43 +0700

    Initial commit 2

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Stepan Potapov on 12.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
